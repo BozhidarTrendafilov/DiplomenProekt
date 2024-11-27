0b0ac83f2e287d817719a45b737f111f4dffe5e8
BuildTools <unconfigured@null.spigotmc.org>
2024-11-27 09:35:18 +0200

    Add project files.

---
+++ DiplomenProekt/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using DiplomenProekt.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DiplomenProekt.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult contact()
+        {
+            return View();
+        }
+
+        public IActionResult faq() 
+        { 
+            return View();
+        }
+
+        public IActionResult pricing()
+        {
+            return View();
+        }
+
+        public IActionResult _LoginPartial()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
